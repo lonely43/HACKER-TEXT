de08c69d7b97789b3e9ba97933849c859c28569a
lonely43 <iladenisov327@gmail.com>
2023-05-05 15:07:24 +0300

    2-option

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const background = document.querySelector(".container");
 const text = document.getElementById("text");
 
-let delta = null;
+let delta = 0;
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const words = ["LIVE","KILL UR SELF","FORGIVE","KILL","CRY","NEVERMIND","FORGET"]
 
 setInterval(() => {
    background.classList.toggle("dark")
+   delta += 1
+   //text.innerText = text.innerText.split("").map((i) => {
+   //   return letters[Math.floor(Math.random() * 26)]
+   //}).join("")
 
-   text.innerText = text.innerText.split("").map((letter, index) => {
-      return letters[Math.floor(Math.random() * 26)]
-   }).join("")
-}, 30)
+   if(delta >= words.length){
+      delta = 0
+   }
+   text.innerText = words[delta]
+}, 80)
