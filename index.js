6525de87a53af1bc679299bfcecd57da484b4c9d
lonely43 <iladenisov327@gmail.com>
2023-05-05 15:27:02 +0300

    fix

--- index.js
+++ index.js
@@ -3,30 +3,18 @@ const text = document.getElementById("text");
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+//let delta = 0;
+//const words = ["LIVE","KILL UR SELF","FORGIVE","KILL","CRY","NEVERMIND","FORGET"]
+
 setInterval(() => {
    background.classList.toggle("dark")
+   //delta += 1
+   //   if(delta >= words.length){
+   //   delta = 0
+   //}
+   //text.innerText = words[delta]
 
    text.innerText = text.innerText.split("").map((letter, index) => {
       return letters[Math.floor(Math.random() * 26)]
    }).join("")
 }, 30)
-
-
-/*
-const background = document.querySelector(".container");
-const text = document.getElementById("text");
-
-let delta = 0;
-
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const words = ["LIVE","KILL UR SELF","FORGIVE","KILL","CRY","NEVERMIND","FORGET"]
-
-setInterval(() => {
-   background.classList.toggle("dark")
-   delta += 1
-
-   if(delta >= words.length){
-      delta = 0
-   }
-   text.innerText = words[delta]
-*/
