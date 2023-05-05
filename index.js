5e4536527ab1be110e091717766f3a830e44a035
lonely43 <iladenisov327@gmail.com>
2023-05-05 15:26:49 +0300

    fix

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ setInterval(() => {
       return letters[Math.floor(Math.random() * 26)]
    }).join("")
 }, 30)
+
+
+/*
+const background = document.querySelector(".container");
+const text = document.getElementById("text");
+
+let delta = 0;
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const words = ["LIVE","KILL UR SELF","FORGIVE","KILL","CRY","NEVERMIND","FORGET"]
+
+setInterval(() => {
+   background.classList.toggle("dark")
+   delta += 1
+
+   if(delta >= words.length){
+      delta = 0
+   }
+   text.innerText = words[delta]
+*/
