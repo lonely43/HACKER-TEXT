312cc8e8464832d277b43322ae2f1482e36e0086
lonely43 <iladenisov327@gmail.com>
2023-05-05 14:50:37 +0300

    1 - option

---
+++ index.js
@@ -0,0 +1,14 @@
+const background = document.querySelector(".container");
+const text = document.getElementById("text");
+
+let delta = null;
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+setInterval(() => {
+   background.classList.toggle("dark")
+
+   text.innerText = text.innerText.split("").map((letter, index) => {
+      return letters[Math.floor(Math.random() * 26)]
+   }).join("")
+}, 30)
